4e4671d7c38a27951a9bffb1628e691b9cc89256
ThanushanR <thanush883@gmail.com>
2019-07-06 11:00:02 +0530

    recruitment

---
+++ src/test/java/com/sgic/automation/leaveSystem/tests/Recuritment/AddRecuritment.java
@@ -0,0 +1,18 @@
+package com.sgic.automation.leaveSystem.tests.Recuritment;
+
+
+import com.sgic.automation.leaveSystem.pages.DashboardPage;
+import com.sgic.automation.leaveSystem.pages.RecruitmentTypePage;
+import com.sgic.automation.leaveSystem.utils.TestBase;
+import org.testng.annotations.Test;
+
+public class AddRecuritment extends TestBase {
+
+    @Test()
+    public void addRecruitementType() {
+        DashboardPage.clickManageEpmloyeeMenu();
+        DashboardPage.clickManageRecuritmentSubMenu();
+        RecruitmentTypePage.AddRecruitmentData();
+
+    }
+}
